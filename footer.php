a21512694fd59485f4a740bd0264875dc5aecc14
inatsuki65 <inatsuki65@gmail.com>
2020-05-22 22:16:18 +0900

    2020/05/22 commit

--- footer.php
+++
@@ -1,5 +0,0 @@
-    <footer class="footer">
-      <p class="footer__copyright"><small>© Raise Tech task</small></p>
-    </footer>
-  </body>
-</html>
--- index.php
+++
@@ -1,9 +0,0 @@
-﻿<?php get_header(); ?>
-  <body>
-    <div class = "damysite">
-      <img src="<?php bloginfo('template_directory'); ?>/img/3ba5a44414bb3c0885c27ff614596431.png" class="damy_site_img">
-      <p class="damysite_mesg">ダミーサイト<p>
-    </div>
-<?php get_footer(); ?>
-
-
